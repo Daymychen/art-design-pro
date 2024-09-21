87a993b6fa3aa77342ae2add6a10be1c9c62df10
涛 陈 <252567138@qq.com>
2024-09-22 00:24:38 +0800

    chore: upgrade the pop-up window to add the upgrade content

---
+++ src/mock/upgradeLog.ts
@@ -0,0 +1,163 @@
+export const upgradeLogList = ref([
+  {
+    version: 'v1.0.33',
+    title: '新增项目文档',
+    date: '2024-9-21',
+    status: 'complete',
+    statusText: '完成'
+  },
+  {
+    version: 'v1.0.32',
+    title: '菜单折叠持久化存储',
+    date: '2024-9-18',
+    status: 'complete',
+    statusText: '完成'
+  },
+  {
+    version: 'v1.0.31',
+    title: '去除点击菜单按钮页面刷新问题',
+    date: '2024-9-18',
+    status: 'complete',
+    statusText: '完成'
+  },
+  {
+    version: 'v1.0.3',
+    title: '新增页面切换动画',
+    date: '2024-9-17',
+    status: 'complete',
+    statusText: '完成'
+  },
+  {
+    version: 'v1.0.27',
+    title: '菜单标题多语言封装',
+    date: '2024-9-13',
+    status: 'complete',
+    statusText: '完成'
+  },
+  {
+    version: 'v1.0.26',
+    title: 'windows下全局搜索图标优化',
+    date: '2024-9-13',
+    status: 'complete',
+    statusText: '完成'
+  },
+  {
+    version: 'v1.0.25',
+    title: '新增网络异常提示组件',
+    date: '2024-9-12',
+    status: 'complete',
+    statusText: '完成'
+  },
+  {
+    version: 'v1.0.24',
+    title: 'CSS主题变量重构',
+    date: '2024-9-11',
+    status: 'complete',
+    statusText: '完成'
+  },
+  {
+    version: 'v1.0.23',
+    title: '全局搜索新增历史记录',
+    date: '2024-9-9',
+    status: 'complete',
+    statusText: '完成'
+  },
+  {
+    version: 'v1.0.22',
+    title: '头像菜单升级',
+    date: '2024-9-9',
+    status: 'complete',
+    statusText: '完成'
+  },
+  {
+    version: 'v1.0.21',
+    title: '搜索升级为全局弹窗',
+    date: '2024-9-7',
+    status: 'complete',
+    statusText: '完成'
+  },
+  {
+    version: 'v1.0.2',
+    title: '搜索支持键盘上下选择、回车搜索',
+    date: '2024-9-5',
+    status: 'complete',
+    statusText: '完成'
+  },
+  {
+    version: 'v1.0.1',
+    title: '留言板块开发',
+    date: '2024-9-4',
+    status: 'complete',
+    statusText: '完成'
+  },
+  {
+    version: 'v1.0.0（正式版）',
+    title: '基础功能开发完成',
+    date: '2024-9-3',
+    status: 'complete',
+    statusText: '完成'
+  },
+  {
+    version: 'v1.0.0',
+    title: 'cz-git 实现 git 可视化提交',
+    date: '2024-8-27',
+    status: 'complete',
+    statusText: '完成'
+  },
+  {
+    version: 'v1.0.0',
+    title: 'Husky + CommitLint 代码提交自动格式化',
+    date: '2024-8-26',
+    status: 'complete',
+    statusText: '完成'
+  },
+  {
+    version: 'v1.0.0',
+    title: 'Perttier、StyleLint 代码格式化&验证',
+    date: '2024-8-25',
+    status: 'complete',
+    statusText: '完成'
+  },
+  {
+    version: 'v1.0.0',
+    title: 'Eslint 代码校验',
+    date: '2024-8-25',
+    status: 'complete',
+    statusText: '完成'
+  },
+  {
+    version: 'v1.0.0',
+    title: 'LocalStorage 系统数据校验',
+    date: '2024-8-24',
+    status: 'complete',
+    statusText: '完成'
+  },
+  {
+    version: 'v1.0.0',
+    title: '多语言支持',
+    date: '2024-8-23',
+    status: 'complete',
+    statusText: '完成'
+  },
+  {
+    version: 'v1.0.0',
+    title: '主题跟随系统切换',
+    date: '2024-8-22',
+    status: 'complete',
+    statusText: '完成'
+  },
+  {
+    version: 'v1.0.0',
+    title: '多标签支持左右滚动',
+    date: '2024-8-21',
+    status: 'complete',
+    statusText: '完成'
+  },
+  {
+    version: 'v1.0.0',
+    title: '项目依赖版本升级',
+    date: '2024-8-20',
+    status: 'complete',
+    statusText: '完成'
+  }
+])
--- src/utils/upgrade.ts
+++ src/utils/upgrade.ts
@@ -1,10 +1,12 @@
-import { useSettingStore } from '@/store/modules/setting'
+import { upgradeLogList } from '@/mock/upgradeLog'
 import { ElNotification } from 'element-plus'
 
 // 检测系统升级
 export function checkSystemUpgrade() {
   setTimeout(() => {
     const version = import.meta.env.VITE_VERSION
+    const content = upgradeLogList.value[0].title
+    const message = `<p style="color: #333; padding-bottom: 5px;">系统已升级到 ${version} 版本，此次更新带来了以下改进：</p>${content}`
 
     // 跳过版本为1.0.0的提示
     if (version === '1.0.0') return
@@ -15,29 +17,19 @@ export function checkSystemUpgrade() {
 
     if (oldSysKey) {
       ElNotification({
-        title: '系统升级',
-        message: `检测到系统已更新到 ${version} 版本，正在升级中...`,
-        type: 'warning',
+        title: '系统升级公告',
+        message,
         duration: 0,
-        offset: 50
+        type: 'success',
+        dangerouslyUseHTMLString: true
       })
 
       setTimeout(() => {
+        // 更新版本号
         localStorage.setItem('version', version)
+        // 删除旧版本号
         localStorage.removeItem(oldSysKey)
-
-        ElNotification({
-          title: '升级成功',
-          message: `系统已升级到 ${version} 版本`,
-          type: 'success',
-          duration: 0,
-          offset: 50
-        })
-
-        setTimeout(() => {
-          useSettingStore().reload()
-        }, 1000)
-      }, 3000)
+      }, 1000)
     }
   }, 1000)
 }
