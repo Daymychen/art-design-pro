370849d021bd4bc25d66a28c3dfc170f8308fe30
百里飞洋 <Barry-Flynn@qq.com>
2025-06-15 20:28:56 +0800

    refactor: supplement the menu interface for backend control mode in menuApi.ts

--- src/types/auto-imports.d.ts
+++ src/types/auto-imports.d.ts
@@ -7,13 +7,7 @@
 export {}
 declare global {
   const EffectScope: (typeof import('vue'))['EffectScope']
-  const ElButton: (typeof import('element-plus/es'))['ElButton']
   const ElMessage: (typeof import('element-plus/es'))['ElMessage']
-  const ElMessageBox: (typeof import('element-plus/es'))['ElMessageBox']
-  const ElNotification: (typeof import('element-plus/es'))['ElNotification']
-  const ElPopconfirm: (typeof import('element-plus/es'))['ElPopconfirm']
-  const ElPopover: (typeof import('element-plus/es'))['ElPopover']
-  const ElTableColumn: (typeof import('element-plus/es'))['ElTableColumn']
   const ElTag: (typeof import('element-plus/es'))['ElTag']
   const acceptHMRUpdate: (typeof import('pinia'))['acceptHMRUpdate']
   const asyncComputed: (typeof import('@vueuse/core'))['asyncComputed']
