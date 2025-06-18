af96cd30e90a4c4f9ea86054818c9b191354390d
百里飞洋 <Barry-Flynn@qq.com>
2025-06-18 09:15:48 +0800

    refactor: 后端控制模式切换成模拟接口返回的菜单数据

--- src/api/menuApi.ts
+++ src/api/menuApi.ts
@@ -1,12 +1,12 @@
-import { Api } from '@/typings/api'
+// import { Api } from '@/typings/api'
 import { menuDataToRouter } from '@/router/utils/menuToRouter'
 import { AppRouteRecord } from '@/types/router'
 
 // 后端控制模式（模拟接口返回的菜单数据）
-// import { asyncRoutes } from '@/router/routes/asyncRoutes'
+import { asyncRoutes } from '@/router/routes/asyncRoutes'
 
 // 后端控制模式（获取真实接口返回的菜单数据）
-import request from '@/utils/http'
+// import request from '@/utils/http'
 
 interface MenuResponse {
   menuList: AppRouteRecord[]
@@ -16,34 +16,34 @@ interface MenuResponse {
 // 下方代码仅在权限控制模式为“后端控制模式”时生效
 export const menuService = {
   // 后端控制模式（模拟接口返回的菜单数据）
-  // async getMenuList(delay = 300): Promise<MenuResponse> {
-  //   try {
-  //     // 模拟接口返回的菜单数据
-  //     const menuData = asyncRoutes
-  //     // 处理菜单数据
-  //     const menuList = menuData.map((route) => menuDataToRouter(route))
-  //     // 模拟接口延迟
-  //     await new Promise((resolve) => setTimeout(resolve, delay))
-
-  //     return { menuList }
-  //   } catch (error) {
-  //     throw error instanceof Error ? error : new Error('获取菜单失败')
-  //   }
-  // }
-
-  // 后端控制模式（获取真实接口返回的菜单数据）
-  async getMenuList(): Promise<MenuResponse> {
+  async getMenuList(delay = 300): Promise<MenuResponse> {
     try {
-      // 获取菜单列表
-      const { menuList: menuData = [] } = await request.get<Api.User.MenuList>({
-        url: '/api/user/menu'
-      })
+      // 模拟接口返回的菜单数据
+      const menuData = asyncRoutes
       // 处理菜单数据
       const menuList = menuData.map((route) => menuDataToRouter(route))
+      // 模拟接口延迟
+      await new Promise((resolve) => setTimeout(resolve, delay))
+
       return { menuList }
     } catch (error) {
-      console.error('获取菜单列表失败:', error)
-      throw error instanceof Error ? error : new Error('获取菜单列表失败')
+      throw error instanceof Error ? error : new Error('获取菜单失败')
     }
   }
+
+  // 后端控制模式（获取真实接口返回的菜单数据）
+  // async getMenuList(): Promise<MenuResponse> {
+  //   try {
+  //     // 获取菜单列表
+  //     const { menuList: menuData = [] } = await request.get<Api.User.MenuList>({
+  //       url: '/api/user/menu'
+  //     })
+  //     // 处理菜单数据
+  //     const menuList = menuData.map((route) => menuDataToRouter(route))
+  //     return { menuList }
+  //   } catch (error) {
+  //     console.error('获取菜单列表失败:', error)
+  //     throw error instanceof Error ? error : new Error('获取菜单列表失败')
+  //   }
+  // }
 }
